cee4163735a75bfe7ae0b4116d338d9d9a48debe
wangheng3 <wangheng3@360.cn>
2022-09-25 22:28:54 +0800

    feat: 解决editor上传失败问题

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -17,6 +17,47 @@ const uploadImageCallBack = async (file: any)=>{
 }
 
 
+interface MediaProps {
+	contentState: any;
+	block: any;
+	blockProps: any;
+}
+
+interface Sate {}
+
+export const myBlockRenderer = (contentBlock: any): any => {
+	const type = contentBlock.getType();
+
+	// 图片类型转换为mediaComponent
+	if (type === 'atomic') {
+		return {
+			component: Media,
+			editable: false,
+			props: {
+				foo: 'bar',
+			},
+		};
+	}
+};
+
+class Media extends Component<MediaProps, Sate> {
+	render() {
+		const { block, contentState } = this.props;
+		const data = contentState.getEntity(block.getEntityAt(0)).getData();
+		const emptyHtml = ' ';
+		return (
+			<div>
+				{emptyHtml}
+				<img
+					src={data.src}
+					alt={data.alt || ''}
+					style={{ height: data.height || 'auto', width: data.width || 'auto' }}
+				/>
+			</div>
+		);
+	}
+}
+
 interface Props {
   value?: string;
   onChange?: (value: string) => void;
@@ -51,6 +92,7 @@ const MyEditor: React.FC<Props> = ({ value, onChange }) => {
         toolbarClassName={styles['toolbar-class']}
         wrapperClassName={styles['wrapper-class']}
         editorClassName={styles['editor-class']}
+        customBlockRenderFunc={myBlockRenderer}
         onEditorStateChange={onEditorStateChange}
         toolbar={{
           options: [
